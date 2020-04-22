744840be19eb6d8b31b295e3b1e78679a0fa74c6
Aytekin Yaliz <aytekinyaliz@gmail.com>
2020-04-22 02:23:40 +0100

    update

--- src/modules/course/CourseModule.ts
+++ src/modules/course/CourseModule.ts
@@ -4,10 +4,10 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { CourseController } from "./controllers/CourseController";
 import { CourseDomain } from "./domains/CourseDomain";
 import { CourseRepository } from "./repositories/CourseRepository";
-import { courseSchema } from "./repositories/courseModel";
+import { courseSchema as courseSchemaInstance } from "./repositories/courseModel";
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: "Course", schema: courseSchema }])],
+  imports: [MongooseModule.forFeature([{ name: "Course", schema: courseSchemaInstance }])],
   controllers: [CourseController],
   providers: [CourseDomain, CourseRepository]
 })
